e8c87d767a95c5614c87db609c79d9b4787f4391
Aleksey Semenov <7.on.off@bk.ru>
2020-03-29 21:58:24 +0300

    Реализация.
    Добавляет создание таблицы education.

--- tools/create_db.py
+++ tools/create_db.py
@@ -14,9 +14,9 @@ def create_db():
                     cur.execute(
                         '''
                         CREATE TABLE student(
-                        id serial PRIMARY KEY,
-                        name varchar(100),
-                        gpa numeric(10, 2),
+                        id serial PRIMARY KEY ,
+                        name varchar(100) NOT NULL,
+                        gpa numeric(10, 2) ,
                         birth timestamp with time zone
                         );
 
@@ -27,12 +27,24 @@ def create_db():
                     cur.execute(
                         '''
                         CREATE TABLE course(
-                        id serial PRIMARY KEY,
-                        name varchar(100)
+                        id serial PRIMARY KEY ,
+                        name varchar(100) NOT NULL
                         );
 
                         '''
                     )
+
+                    cur.execute(
+                        '''
+                        CREATE TABLE education(
+                        id serial PRIMARY KEY,
+                        course_id INTEGER REFERENCES course(id) NOT NULL,
+                        student_id INTEGER REFERENCES student(id) NOT NULL
+                        );
+
+                        '''
+                    )
+
                 except pg.Error as e:
                     print(e.pgerror)
                     return False
