1d3a28ab44a9aa6739c3201041401864b81f245e
Aleksey Semenov <7.on.off@bk.ru>
2020-03-30 00:48:44 +0300

    Реализация.
    Добавляет запрос студента из базы.

---
+++ tools/get_student.py
@@ -0,0 +1,33 @@
+import psycopg2 as pg
+from config import HOST, PORT, DB, DB_USER, DB_PASSWORD
+
+
+def get_student(student):
+    """
+    cоздает студента
+    """
+    try:
+        with pg.connect(
+                f'host={HOST} '
+                f'port={PORT} '
+                f'dbname={DB} '
+                f' user={DB_USER} '
+                f'password={DB_PASSWORD} '
+        ) as conn:
+            with conn.cursor() as cur:
+                try:
+                    cur.execute(
+                        '''
+                        SELECT * FROM student WHERE name = %s
+                        ''', (student['name'],)
+
+                    )
+
+                    return tuple(cur)
+                except pg.Error as e:
+                    print(e.pgerror)
+                    return False
+    except pg.Error as e:
+        print('Ошибка подключения к базе данных! Проверьте настройки подключения.')
+        return False
+    return True
