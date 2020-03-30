ebf8f2d756d0feb3cb50e25a0579b1d6125f7dc0
Aleksey Semenov <7.on.off@bk.ru>
2020-03-30 17:02:32 +0300

    Рефакторинг.
    Меняет код:
      tools/add_student.py
      tools/get_student.py

--- tools/add_student.py
+++ tools/add_student.py
@@ -1,33 +1,12 @@
-import psycopg2 as pg
-from config import HOST, PORT, DB, DB_USER, DB_PASSWORD
+from tools import queryes
+from tools.get_executed_query import get_executed_query as geq
 
 
-def add_student(student):
+def add_student(student: dict):
     """
-    cоздает студента
+    Добавляет студента в базу.
+
+    :param student: dict
+    :return: Bool
     """
-    try:
-        with pg.connect(
-                f'host={HOST} '
-                f'port={PORT} '
-                f'dbname={DB} '
-                f' user={DB_USER} '
-                f'password={DB_PASSWORD} '
-        ) as conn:
-            with conn.cursor() as cur:
-                try:
-                    cur.execute(
-                        '''
-                        INSERT INTO student (name, birth) 
-                        values (%s, %s)
-                        ''', (student['name'], student['birth'])
-
-                    )
-
-                except pg.Error as e:
-                    print(e.pgerror)
-                    return False
-    except pg.Error as e:
-        print('Ошибка подключения к базе данных! Проверьте настройки подключения.')
-        return False
-    return True
+    return geq(queryes.add_student, student['name'], student['birth'])
--- tools/get_student.py
+++ tools/get_student.py
@@ -1,4 +1,11 @@
 from tools import queryes
 from tools.get_executed_query import get_executed_query as geq
 
-get_student = lambda student: geq(queryes.get_student, student)
+
+def get_student(student_id: int):
+    """
+    Получает студента из базы.
+    :param student_id: int
+    :return: tuple или False в случае неудачи
+    """
+    return geq(queryes.get_student, student_id)
